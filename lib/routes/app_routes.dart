66dbac644a5c7bf43c5d7dd7ebe2a0160faaf3c4
LENOVO <yudhaaryandi03@gmail.com>
2024-11-08 00:02:17 +0700

    slicing introduction first and second step

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,12 +2,14 @@ import 'package:get/get.dart';
 import 'package:virtual_assistant/auth/login.dart';
 import 'package:virtual_assistant/auth/register.dart';
 import 'package:virtual_assistant/splash_screen.dart';
+import 'package:virtual_assistant/layout/first_step.dart';
 
 class AppRoutes {
   static const splash = '/';
   static const login = '/login';
   static const home = '/home';
   static const register = '/register';
+  static const first_step = '/first_step';
 
   static final routes = [
     GetPage(name: splash, page: () => SplashScreen()),
